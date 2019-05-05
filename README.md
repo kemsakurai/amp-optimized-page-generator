bc022c2e97c2d5d1e2fab64fa28f4a8f18757a0d
kemsakuai <sakurai.kem@gmail.com>
2019-05-06 00:11:57 +0900

    Init comit

---
+++ README.md
@@ -0,0 +1,5 @@
+# amp-optimized-page-generator       
+
+[ブログ | Monotalk](https://www.monotalk.xyz/) の AMPページのHTML を取得して、    
+[amp-toolbox/packages/optimizer at master · ampproject/amp-toolbox](https://github.com/ampproject/amp-toolbox/tree/master/packages/optimizer) で変換した HTML を保存するツールです。     
+
---
+++ index.js
@@ -0,0 +1,88 @@
+const ampOptimizer = require('amp-toolbox-optimizer');
+const https = require('https');
+const xml2js = require('xml2js');
+const parser = new xml2js.Parser({ attrkey: 'ATTR' });
+const url = 'https://www.monotalk.xyz/sitemap.xml';
+const fs = require('fs');
+// retrieve the latest runtime version
+const runtimeVersion = require('amp-toolbox-runtime-version');
+
+function getAmpRuntimeVersion() {
+  const p = new Promise((resolve) => {  
+    let ampRuntimeVersion = runtimeVersion.currentVersion();  
+    resolve(ampRuntimeVersion)
+  });
+  return p;
+}
+
+function getAmpUrlsFromSitemap() {
+  const p = new Promise((resolve, reject) => {  
+    https.get(url, function(res) {
+        let data = '';
+        res.on('data', function(stream) {
+            data += stream;
+        });
+        res.on('end', function(){
+            parser.parseString(data, function(error, result) {
+                if(error === null) {
+                    let resultUrls = []
+                    for (const elem of result.urlset.url) {
+                      resultUrls.push(elem.loc[0].replace('https://www.monotalk.xyz/', 'https://www.monotalk.xyz/amp/'));
+                    }
+                    resolve(resultUrls);
+                }
+                else {
+                    reject(error);
+                }
+            });
+        });
+    });  
+  });
+  return p;
+}
+
+function createTransformedHtml(url) {
+  const elems = url.split('/');
+  let fileName = elems[elems.length -2];
+  fileName = fileName.slice(0, 100) + '.html';
+  const canonicalUrl = url.replace('https://www.monotalk.xyz/amp/', 'https://www.monotalk.xyz/');
+  const p = new Promise((resolve) => {
+    https.get(url, function(res) {
+        let data = '';
+        res.setEncoding('utf8');
+        
+        res.on('data', function(stream) {
+            data += stream;
+        });
+        res.on('end', function() {
+            getAmpRuntimeVersion().then((ampRuntimeVersion) => {
+                // Additional options can be passed as the second argument
+                ampOptimizer.transformHtml(data, {
+                  ampUrl: canonicalUrl,
+                  ampRuntimeVersion: ampRuntimeVersion
+                }).then((optimizedHtml) => {
+                  fs.writeFile('./htmls/' + fileName, optimizedHtml, 'utf8', (error) => {
+                    if (error) {
+                      console.log(JSON.stringify(error));
+                    }
+                  });
+                  resolve(fileName);
+                  /* eslint-disable-next-line no-console */
+                }).catch((e) => console.log(e));
+            })
+        });
+    });
+  });
+  return p;
+}
+
+const promise = getAmpUrlsFromSitemap();
+promise.then((resultUrls) => {
+  for (let url of resultUrls) {
+    createTransformedHtml(url).then((filename) => {
+      /* eslint-disable-next-line no-console */
+      console.log(filename + ' WRITE DONE')
+    })
+  }
+  /* eslint-disable-next-line no-console */
+}).catch((e) => console.log(e));
